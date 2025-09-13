068ec1351ff3567a09b3eb97ccd97a615716a5de
Artyom <Artyom.Masyuk@mail.ru>
2025-09-13 14:13:21 +0300

    Сделал дополнительное задание

--- labik1/labik1/labik1.cpp
+++ labik1/labik1/labik1.cpp
@@ -17,6 +17,10 @@ struct student
     int Nomzach;
 };
 
+int partial_match(const char* str1, const char* str2) {
+    return strstr(str1, str2) != NULL;
+}
+
 int main(void)
 {
     setlocale(LC_ALL, "Russian");
@@ -146,39 +150,38 @@ int main(void)
 
     printf("\nЗАДАНИЕ 5:\n\n");
 
-    struct student stud[3];
+    struct student stud[4];
     int found = 0;
-    char search_famil[20];
-    char search_name[20];
+    char search_string[20];
     int search_nomzach;
     int search_choice;
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("Введите фамилию студента %d: ", i + 1);
         scanf("%19s", stud[i].famil);
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("Введите имя студента %s: ", stud[i].famil);
         scanf("%19s", stud[i].name);
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("Введите название факультета студента %s %s: ", stud[i].famil, stud[i].name);
         scanf("%19s", stud[i].facult);
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("Введите номер зачётной книжки студента %s %s: ", stud[i].famil, stud[i].name);
         scanf("%d", &stud[i].Nomzach);
     }
 
     printf("\nСписок всех студентов:\n");
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("Студент %s %s обучается на факультете %s, номер зачётной книжки %d \n",
             stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
@@ -195,10 +198,10 @@ int main(void)
     {
     case 1:
         printf("Введите фамилию для поиска: ");
-        scanf("%19s", search_famil);
-        for (i = 0; i < 3; i++)
+        scanf("%19s", search_string);
+        for (i = 0; i < 4; i++)
         {
-            if (strcmp(stud[i].famil, search_famil) == 0)
+            if (partial_match(stud[i].famil, search_string))
             {
                 printf("Найден студент: %s %s, факультет: %s, номер: %d\n",
                     stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
@@ -209,10 +212,10 @@ int main(void)
 
     case 2:
         printf("Введите имя для поиска: ");
-        scanf("%19s", search_name);
-        for (i = 0; i < 3; i++)
+        scanf("%19s", search_string);
+        for (i = 0; i < 4; i++)
         {
-            if (strcmp(stud[i].name, search_name) == 0)
+            if (partial_match(stud[i].name, search_string))
             {
                 printf("Найден студент: %s %s, факультет: %s, номер: %d\n",
                     stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
@@ -224,7 +227,7 @@ int main(void)
     case 3:
         printf("Введите номер зачетной книжки: ");
         scanf("%d", &search_nomzach);
-        for (i = 0; i < 3; i++)
+        for (i = 0; i < 4; i++)
         {
             if (stud[i].Nomzach == search_nomzach)
             {
@@ -240,10 +243,50 @@ int main(void)
         break;
     }
 
-    if (!found && search_choice >= 1 && search_choice <= 3)
+    if (!found && search_choice >= 1 && search_choice <= 4)
     {
         printf("Студент не найден!\n");
     }
+
+    printf("\nДОП.ЗАДАНИЕ:\n\n");
+
+    int matrix_size;
+    printf("Введите размер квадратной матрицы: ");
+    scanf("%d", &matrix_size);
+
+    int** dynamic_matrix = (int**)malloc(matrix_size * sizeof(int*));
+    for (i = 0; i < matrix_size; i++) {
+        dynamic_matrix[i] = (int*)malloc(matrix_size * sizeof(int));
+    }
+
+    for (i = 0; i < matrix_size; i++) {
+        for (j = 0; j < matrix_size; j++) {
+            dynamic_matrix[i][j] = 2 + rand() % 4;
+        }
+    }
+
+    for (i = 0; i < matrix_size; i++) {
+        dynamic_matrix[i][i] = 0;
+    }
+
+    for (i = 0; i < matrix_size; i++) {
+        for (j = i + 1; j < matrix_size; j++) {
+            dynamic_matrix[j][i] = dynamic_matrix[i][j];
+        }
+    }
+
+    printf("\nРезультирующая матрица:\n");
+    for (i = 0; i < matrix_size; i++) {
+        for (j = 0; j < matrix_size; j++) {
+            printf("%3d ", dynamic_matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < matrix_size; i++) {
+        free(dynamic_matrix[i]);
+    }
+    free(dynamic_matrix);
 
     system("pause");
     return 0;
